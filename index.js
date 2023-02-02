51048b723c18da254f1636bdfdc9637eaee2ba90
DeanStavenuiter <info@deanstavenuiter.nl>
2023-02-02 13:52:51 +0100

    finished exercises

--- index.js
+++ index.js
@@ -1,9 +1,88 @@
 //Make sure to require your model so you can connect to the DB
 const { Model } = require("mongoose");
 
+const VikingModel = require("./models/User.js");
+
 //This line of code connects to the Database
 require("./db");
 
 //Iteration 2:
 
 //Iteration 3:
+// const Ragnar = {
+//     firstName: "Ragnar",
+//     familyName: "Lodbrok",
+//     strength: 800,
+//     weapon: "Axe"
+// }
+
+let vikingArr = [
+  {
+    firstName: "Ragnar",
+    familyName: "Lodbrok",
+    strength: 999,
+    weapon: "Axe",
+  },
+  {
+    firstName: "Floki",
+    familyName: "No",
+    strength: 400,
+    weapon: "Knife",
+  },
+  {
+    firstName: "Lagertha",
+    familyName: "Lodbrok",
+    strength: 700,
+    weapon: "Bows and arrows",
+  },
+  {
+    firstName: "Rollo",
+    familyName: "No",
+    strength: 850,
+    weapon: "Sword",
+  },
+  {
+    firstName: "Bjorn",
+    familyName: "Lodbrok",
+    strength: 800,
+    weapon: "Axe",
+  },
+];
+//create a viking
+// VikingModel.create(Ragnar)
+// .then((newVikinginDb) => {
+//     console.log('Nice, now you got a new viking', newVikinginDb)
+// }).catch((error) => {
+//     console.log("There was an error", error)
+// });
+
+// delete viking
+// VikingModel.findByIdAndDelete('63db9dbac9c391dde44689df')
+//   .then((deletedViking) => {
+//     console.log("Here is a object of the deleted viking", deletedViking);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//insert array into the database
+// VikingModel.insertMany(vikingArr)
+// .then((vikingArrDb) => {
+//     console.log("the array was created", vikingArrDb)
+// })
+// .catch((error) => {
+//     console.log(error)
+// })
+
+//update one viking
+VikingModel.findOneAndUpdate(
+  { name: "Ragnar" },
+  {strength: 1000},
+  { new: true } //gives the new schema in the console
+)
+  .then((updateViking) => {
+    console.log("Nice, now you updated", updateViking);
+  })
+  .catch((error) => {
+    console.log("There was an error", error);
+  });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,26 @@
 // require mongoose and destructor Schema and model from it
 const { Schema, model } = require("mongoose");
 //Create a viking schema here
+
+const vikingSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    familyName: {
+        type: String,
+        required: true,
+    },
+    strength: Number,
+    weapong: String,
+});
+
+//creates the model first argument is the name of the collection and the second is the name of the model you want to use
+const VikingModel = model('viking', vikingSchema);
+
+//exports model 
+module.exports = VikingModel
+
+
 //<======Don't forget Module.export ============>
 //<================Schema=====================>
